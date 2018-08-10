da07a0181e56484647917bf07d875c16065013a9
j <jlevitin@gmail.com>
2018-08-09 21:07:32 -0700

    Refactored to use multiple processes

--- server/index.js
+++ server/index.js
@@ -2,25 +2,27 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const { router } = require('../router/index.js')
 const path = require('path')
-const app = express()
-const server = require('http').Server(app)
 const cors = require('cors')
+const cluster = require('cluster')
 
 const port = process.env.PORT || 1336
 
-app.use(cors())
-app.options('*', cors())
-
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json())
-
-app.use(express.static(path.join(__dirname, "../dist")))
-
-app.use("/api", router)
-
-server.listen(port, () => {
-  console.log('server is listening on port', port)
-})
-
-
-
+if(cluster.isMaster) {
+  let workers = require('os').cpus();
+  workers.forEach(t => {
+    cluster.fork()
+  })
+  cluster.on('exit', (worker, code, signal) => {
+    console.log(`Worker ${worker.process.pid} died. Code: ${code} Signal: ${signal}`)
+    cluster.fork()
+  })
+} else {
+  const app = express()
+  app.use(cors())
+  app.options('*', cors())
+  app.use(bodyParser.urlencoded({extended: true}));
+  app.use(bodyParser.json())
+  app.use(express.static(path.join(__dirname, "../dist")))
+  app.use("/api", router)
+    require('http').Server(app).listen(port, () => console.log(`PID ${process.pid} listening on port ${port}`))
+}
