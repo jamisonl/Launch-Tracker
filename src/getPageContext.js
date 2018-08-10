97e28b563a5d5e27298f0f3dd14f529fafb940d9
j <jlevitin@gmail.com>
2018-08-10 12:34:31 -0700

    Injects material-ui for SSR with next

---
+++ src/getPageContext.js
@@ -0,0 +1,38 @@
+import { SheetsRegistry } from 'jss';
+import { createMuiTheme, createGenerateClassName } from '@material-ui/core/styles';
+import blue from '@material-ui/core/colors/blue';
+import green from '@material-ui/core/colors/green';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: blue[300],
+      main: blue[900],
+      dark: blue[700],
+    },
+    secondary: {
+      light: green[300],
+      main: green[500],
+      dark: green[700],
+    },
+  },
+});
+
+function createPageContext() {
+  return {
+    theme,
+    sheetsManager: new Map(),
+    sheetsRegistry: new SheetsRegistry(),
+    generateClassName: createGenerateClassName(),
+  };
+}
+
+export default function getPageContext() {
+  if (!process.browser) {
+    return createPageContext();
+  }
+  if (!global.__INIT_MATERIAL_UI__) {
+    global.__INIT_MATERIAL_UI__ = createPageContext();
+  }
+  return global.__INIT_MATERIAL_UI__;
+}
