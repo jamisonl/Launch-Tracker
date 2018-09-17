5992a5c23a83e96678fb744de8a103f2a80ffa43
Jamison <jlevitin@gmail.com>
2018-09-17 14:11:12 -0700

    Removed nextLaunchController from routes, added new queryLaunchController method

--- router/index.js
+++ router/index.js
@@ -1,19 +1,12 @@
 const router = require('express').Router();
-const { nextLaunchController } = require('../controllers/nextLaunchController')
 const { queryLaunchController } = require('../controllers/queryLaunchController')
 
-  //gets future flights with specific flight number
-router.route('/flight_number_future/:flight_number')
-  .get(nextLaunchController.get)
-router.route('/flight_number_future')
-  .get(nextLaunchController.get)
-
-  //gets past flights, either with specific mission_number or returning all past flights
-
-router.route('/flight_number_history/:flight_number')
+  //gets parameter limited flight data, either searched through related text info or by corresponding flight number
+router.route('/flight_data/:flight_number')
   .get(queryLaunchController.get)
-router.route('/flight_number_history')
-  .get(queryLaunchController.get)
+  //returns all flights
+router.route('/flight_data')
+  .get(queryLaunchController.getAll)
 
 
 
