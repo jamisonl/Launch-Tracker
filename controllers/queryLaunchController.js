624160c14aedfc7b9b3baf7d59d6d16b039d8c11
Jamison <jlevitin@gmail.com>
2018-09-17 14:12:04 -0700

    Moved the get all method out of get for readability

--- controllers/queryLaunchController.js
+++ controllers/queryLaunchController.js
@@ -1,8 +1,16 @@
-const { getRocket } = require('../db')
+const { getRocket, getRockets } = require('../db')
 
 const queryLaunchController = {
   'get' : (req, res) => {
     getRocket(req.params.flight_number, (err, data) => {
+      if(err) {
+        res.status(400).send(err)
+      }
+      res.status(200).send(data)
+    })
+  },
+  'getAll' : (req, res) => {
+    getRockets((err, data) => {
       if(err) {
         res.status(400).send(err)
       }
